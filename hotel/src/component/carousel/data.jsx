c88add69dc4b387a86346a342e3cb9efdf5e6ea9
opeyemi-code <opeyemitoheeb31@gmail.com>
2023-03-13 04:04:49 -1200

    ..

--- hotel/src/component/carousel/data.jsx
+++ hotel/src/component/carousel/data.jsx
@@ -3,7 +3,7 @@ export default [
     {
        id: 1,
        img: '/public/image12.png', 
-       secondimg:'/public/Star 1.png',
+       secondimg:'/public/Star1.png',
        ratings: '5.0 (6) . USA',
        quote: 'life lessons with Katie Zaferes',
        price: 'From $134/ person',
@@ -23,7 +23,7 @@ export default [
      {
         id:3,
         img: '/public/mountain-bike1.png' , 
-        secondimg: '/public/Star 1.png',
+        secondimg: '/public/Star1.png',
         ratings: '5.0 (30). USA',
         quote: 'Group Mountain bikini',
         price: 'From $50/ person',
