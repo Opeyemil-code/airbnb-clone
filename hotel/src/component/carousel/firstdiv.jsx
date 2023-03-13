5b26186ab17f5bcbee0d9a80da867f32a08fae47
opeyemi-code <opeyemitoheeb31@gmail.com>
2023-03-13 00:42:08 -1200

    fixed the image not displaying

--- hotel/src/component/carousel/firstdiv.jsx
+++ hotel/src/component/carousel/firstdiv.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 export default function Card(props) {
      let badgeText
-     if (props.Data.openSpots === 0) {
+     if (props.openSpots === 0) {
         badgeText = 'Sold out'
-     } else if (props.Data.openSpots > 0) {
+     } else if (props.openSpots > 0) {
         badgeText = 'Online' 
      }
-     console.log(props.Data.badgeText)
+     console.log(props.badgeText)
     return(
       <div className="flex flex-col  lg:mx-0 mx-auto">
-            <img src={props.Data.img} className="w-56 h-72 rounded-lg" />
+            <img src={props.fimg} className="w-56 h-72 rounded-lg" />
             <div className="flex flex-row space-x-2">
-                <img src={props.Data.secondimg} className="w-5 h-5" />
+                <img src={props.star} className="w-5 h-5" />
                 <span className=''></span>
                 <span className=""> • </span>
-                <span className="">{props.Data.ratings}</span>
+                <span className="">{props.ratings}</span>
             </div>
-            <p className="font-mono">{props.Data.quote}</p>
-            <p><span className="font-mono"> {props.Data.price} </span></p>
-           <div className="border absolute bg-white px-3 py-1 rounded ml-2 text-sm font-mono font-bold">{badgeText}</div>
+            <p className="font-mono">{props.quote}</p>
+            <p><span className="font-mono"> {props.price} </span></p>
+            <div className="border absolute bg-white px-3 py-1 rounded ml-2 text-sm font-mono font-bold">{badgeText}</div>
         </div>
     )
 }
