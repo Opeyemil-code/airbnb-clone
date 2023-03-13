bd9762ad467a2cebf095b3f2fbb0874572c5cf82
opeyemi-code <opeyemitoheeb31@gmail.com>
2023-03-13 10:29:27 -1200

    done building this specific section

--- hotel/src/component/carousel/firstdiv.jsx
+++ hotel/src/component/carousel/firstdiv.jsx
@@ -9,9 +9,9 @@ export default function Card(props) {
      }
      
     return(
-      <div className="flex flex-col  lg:mx-0 mx-auto  w-52 ">
-            <img src={props.fimg} className="w-56 h-72 rounded-l" />
-            <div className="flex flex-row space-x- text-center">
+      <div className="flex flex-col  lg:mx-0 mx-auto  lg:w-64 w-72 text-center">
+            <img src={props.fimg} className="w-56 h-72 rounded-lg mx-auto" />
+            <div className="flex flex-row space-x- text-center mx-auto mt-2">
                 <img src={props.star} className="w-5 h-5" />
                 <span className=''></span>
                 <span className=""> • </span>
@@ -19,7 +19,7 @@ export default function Card(props) {
             </div>
             <p className="font-mono">{props.quote}</p>
             <p><span className="font-mono"> {props.price} </span></p>
-            <div className="border absolute bg-white px-3 py-1 rounded ml-2 text-sm font-mono font-bold">{badgeText}</div>
+            <div className="border absolute bg-white px-3 py-1 rounded ml-10 text-sm font-mono font-bold ">{badgeText}</div>
         </div>
     )
 }
--- hotel/src/headline.jsx
+++ hotel/src/headline.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Headings(params) {
     return(
-        <div className="w-1/2 mx-auto flex flex-col space-y-3 mt-6">
+        <div className="lg:w-1/2 mx-auto flex flex-col space-y-3 mt-6 w-60 bg-red-90">
             <h1 className="text-4xl font-bold mt-6 tracking-wide">Online Experiences</h1>
-            <p className="font-mono font-bolder">Join unique interactive activities led by <br /> one-of-a-kind hosts-all without leaving <br /> home</p>
+            <p className="font-mono font-bolder w-72 bg-blac lg:w-full">Join unique interactive activities led by <br /> one-of-a-kind hosts-all without leaving <br /> home</p>
         </div>
     )
 }
